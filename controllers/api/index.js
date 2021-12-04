f86f6a97568e846ec09e1c0268649d081867d22c
yanghuixi1 <yanghui.xi1@gmail.com>
2021-12-04 12:40:11 -0500

    Add login, logout, sessions, registration authentication

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const bookRoutes = require("./bookRoutes");
+const userRoutes = require("./userRoutes");
 
 router.use("/books", bookRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,37 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 
 router.use("/api", apiRoutes);
+
+router.get("/", async (req, res) => {
+  res.render("login");
+});
+
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ["password"] },
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render("profile", {
+      users,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("profile");
+    return;
+  }
+  res.render("login");
+});
 
 module.exports = router;
