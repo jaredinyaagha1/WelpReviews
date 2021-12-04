08ae151ca299b42e5c76dc3c348c0f86f1a63165
NIGHT LIGHT <jaredinyaagha@gmail.com>
2021-12-04 11:36:51 -0500

    model index

---
+++ models/index.js
@@ -0,0 +1,21 @@
+const User = require('./User');
+const Book = require('./Book');
+const Review = require('./Review');
+
+User.hasMany(Review, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Book.hasMany(Review, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Review.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
+
+module.exports = { User, Book, Review }
