a91fb9e36f45f847cd39dddcce045f645742d786
NIGHT LIGHT <jaredinyaagha@gmail.com>
2021-12-06 18:53:29 -0500

    user/review relationship defined in models index

--- models/index.js
+++ models/index.js
@@ -1,3 +1,14 @@
 const User = require("./User");
+const Book = require("./Book");
+const Review = require("./Review");
+
+User.hasMany(Review, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Review.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 module.exports = { User };
