8f9a91ec39f3c5662baf6e853c1fb9b1626654ac
Andrew Carroll <andrewc411@yahoo.com>
2021-12-06 16:54:34 -0500

    created tables in schema; created seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,22 @@
 DROP DATABASE IF EXISTS welp_db;
 CREATE DATABASE welp_db;
+
+USE welp_db;
+
+CREATE TABLE books(
+    PRIMARY KEY id INT NOT NULL AUTO_INCREMENT,
+    book_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE reviews(
+    id INT NOT NULL,
+    book_id INT NOT NULL,
+    review TEXT,
+    FOREIGN KEY (book_id)
+    REFERENCES books(id)
+    ON DELETE SET NULL
+)
+
+CREATE TABLE users(
+    id INT NOT NULL,
+)
