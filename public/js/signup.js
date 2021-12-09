ee1174b165921807e2939ad49c268d5d77114fa0
yanghuixi1 <yanghui.xi1@gmail.com>
2021-12-08 23:36:41 -0500

    Fix login issues

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,31 +1,8 @@
-const loginFormHandler = async (event) => {
+const signupFormHandler = async (event) => {
   event.preventDefault();
 
   const email = document.querySelector("#email-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
-
-  if (email && password) {
-    const response = await fetch("/api/users/login", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    // response.status === 200
-    if (response.ok) {
-      document.location.replace("/profile");
-    } else {
-      let message = await response.json();
-      alert(message.message);
-    }
-  }
-};
-
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
 
   if (email && password) {
     const response = await fetch("/api/users/register", {
@@ -51,4 +28,4 @@ const signupFormHandler = async (event) => {
 
 document
   .querySelector(".sign-in-box")
-  .addEventListener("submit", loginFormHandler);
+  .addEventListener("submit", signupFormHandler);
