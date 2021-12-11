e24e7cd2134da1bf3735b179cbeb318b18fe41a8
NIGHT LIGHT <jaredinyaagha@gmail.com>
2021-12-10 21:54:07 -0500

    populating seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,14 @@
 INSERT INTO browseCards (bookName, summaryText, filePath)
-VALUES ()
+ VALUES ("Queenie", "great book great book great book great book","public\images\queenie.jpg" ),
+        ("The Martian", "great book great book great book great book", "public\images\theMartian.jpg"),
+        ("The Toynbee Convector", "great book great book great book great book", "public\images\the-toynbee-convector.jpg"),
+        ("The Remainder", "great book great book great book great book", "public\images\the-remainder.jpg"),
+        ("Females", "great book great book great book great book", "public\images\females.jpg");
+
+
+INSERT INTO bookReviews (username, bookName, reviewText)
+ VALUES ("ironVerdant", "", "great book great book great book great book"),
+        ("finallyTransom", "", "great book great book great book great book"),
+        ("improbableNecklace", "", "great book great book great book great book"),
+        ("bozoHum", "", "great book great book great book great book"),
+        ("boomKneel", "", "great book great book great book great book"),
