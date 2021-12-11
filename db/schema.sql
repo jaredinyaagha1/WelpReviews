d0ddc3ed434114f4f68bd1777630d196c1903deb
Aaron Jones <ronj2005@gmail.com>
2021-12-11 04:51:21 -0500

    update reviews card view/ css

--- db/schema.sql
+++ db/schema.sql
@@ -3,19 +3,19 @@ CREATE DATABASE welp_db;
 
 USE welp_db;
 
-CREATE TABLE browseCards (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    bookName TEXT NOT NULL,
-    summaryText TEXT NOT NULL,
-    filePath TEXT NOT NULL,
-);
+-- CREATE TABLE browseCards (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     bookName TEXT NOT NULL,
+--     summaryText TEXT NOT NULL,
+--     filePath TEXT NOT NULL,
+-- );
 
-CREATE TABLE bookReviews (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    bookName TEXT NOT NULL,
-    summaryText TEXT NOT NULL,
-    filePath TEXT NOT NULL,
-    FOREIGN KEY (seed_id),
-    REFERENCES browseCards(id)
-    ON DELETE SET NULL
-);
+-- CREATE TABLE bookReviews (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     bookName TEXT NOT NULL,
+--     summaryText TEXT NOT NULL,
+--     filePath TEXT NOT NULL,
+--     FOREIGN KEY (seed_id),
+--     REFERENCES browseCards(id)
+--     ON DELETE SET NULL
+-- );
