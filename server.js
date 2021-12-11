56a5ff07d653a650877f1b237fe9cab2e2b241f8
Andrew Carroll <andrewc411@yahoo.com>
2021-12-11 10:00:06 -0500

    changed sync force: true

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 // Starts the server to begin listening
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () =>
     console.log("Server listening on: http://localhost:" + PORT)
   );
